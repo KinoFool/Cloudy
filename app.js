46346ac61830ded761fd65018d2a6af657a4b22f
Nicolas MARY <nicolas.mary@epitech.eu>
2020-05-26 11:37:36 +0200

    Move in folder / download file

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 
-// const mypath = '/mnt/c/Users/nicol/Documents/Code/Web/';
-
 function get_files(mypath) {
     let file_info = [];
 
@@ -12,10 +10,9 @@ function get_files(mypath) {
                 file_info.push({type: 'file', name: file, path: mypath});
             } else if (fs.lstatSync(mypath + file).isDirectory()) {
                 file_info.push({type: 'dir', name: file, path: mypath});
-              }
+            }
         });
         console.log(file_info);
     return file_info;
 }
-
 module.exports = get_files;
--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+var path = require('path')
 var express = require('express')
+var fs = require('fs');
+const os = require('os');
 var app = express()
+
 const get_files = require('./app.js')
+const host_name = os.hostname()
+
+if (host_name == 'dellserver')
+    var mypath = os.homedir()
+else
+    var mypath = '/mnt/c/Users/nicol/Documents/'
+
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', function(req, res) {
-    res.render('pages/index', {files: get_files('/mnt/c/Users/nicol/Documents/Code/Web/')})
+    res.render('pages/index', {files: get_files(mypath)})
 })
 
+app.use('/:file', function(req, res) {
+    if (fs.lstatSync(req.originalUrl).isDirectory()) {
+        console.log('DIR !')
+        // mypath = req.originalUrl + '/'
+        res.render('pages/index', {files: get_files(path.normalize(req.originalUrl + '/'))})
+        return
+    }
+    console.log(req.originalUrl)
+    fs.readFile(req.originalUrl, 'utf8', function (err, data) {
+        if (err) throw err;
+        res.send(data)
+    })
+})
 app.listen(8080, function () {
+    console.log('Platform ==> ' + process.platform)
+    console.log('Hostame ==> '+ os.hostname())
+    console.log('Home ==> ' + os.homedir())
     console.log('Listening on port 8080 !')
 })
